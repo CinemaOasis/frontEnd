1d205dee41ccba159249bf6fc09ce593e95a733b
wandergdev <wanderg.dev@gmail.com>
2024-06-07 14:48:31 -0400

    chore: initial config

---
+++ app/src/components/adminHeader.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import logo from '../assets/Cinema (500 x 200 px).png';
+import '../assets/headerAdmin.css';
+import lupaBuscar from '../assets/lupaBuscar (2).png';
+
+const Header = ({ searchTerm, setSearchTerm, handleSearch, userName }) => {
+    let navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [profileImage, setProfileImage] = useState(null);
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const handleProfileImageChange = (e) => {
+        if (e.target.files && e.target.files[0]) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setProfileImage(e.target.result);
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    };
+
+    return (
+        <header>
+            <div className='headerTop'>
+                <img src={logo} alt="Cinema Oasis logo" className="logo" />
+                <div className="barraBuscar">
+                    <input 
+                        type="text" 
+                        placeholder='Buscar...' 
+                        value={searchTerm} 
+                        onChange={handleInputChange} 
+                        onKeyPress={handleKeyPress} 
+                    />
+                    <button type='button' onClick={handleSearch}>
+                        <img src={lupaBuscar} alt='Buscar' />
+                    </button>
+                </div>
+                <div className='profileMenu'>
+                    <div className="profileIcon" onClick={toggleMenu}>
+                        {profileImage ? (
+                            <img src={profileImage} alt="Perfil" className="profileImage"/>
+                        ) : (
+                            <FontAwesomeIcon icon={faUserCircle} size="2x" />
+                        )}
+                    </div>
+                    {menuOpen && (
+                        <div className='menu'>
+                            <p>{userName}</p>
+                            <label htmlFor="profileImageInput">Cambiar foto de perfil</label>
+                            <input 
+                                id="profileImageInput" 
+                                type="file" 
+                                accept="image/*" 
+                                onChange={handleProfileImageChange} 
+                            />
+                            <button onClick={handleLogout}>Cerrar sesión</button>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            <nav className="headerNav">
+                <Link to="/">Inicio</Link>
+                <Link to="/estrenos">Estrenos</Link>
+                <Link to="/proximamente">Próximamente</Link>
+                <Link to="/contacto">Contacto</Link>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
--- app/src/components/adminPage.js
+++ app/src/components/adminPage.js
@@ -3,6 +3,7 @@ import { Form, Button, Container, Row, Col, Card, ListGroup } from 'react-bootst
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import api from '../services/api';
+import Header from '../components/adminHeader';  // Ajusta la ruta según sea necesario
 
 const AdminPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -12,6 +13,7 @@ const AdminPage = () => {
   const [startTime, setStartTime] = useState('');
   const [cartelera, setCartelera] = useState([]);
   const movieDetailsRef = useRef(null);
+  const [userName, setUserName] = useState('Admin');  // Asigna un nombre de usuario por defecto
 
   useEffect(() => {
     handleLogin();
@@ -24,9 +26,10 @@ const AdminPage = () => {
         email: 'wanderdj77@gmail.com',
         password: '123456789',
       });
-      const { token } = response.data.data;
+      const { token, name } = response.data.data;
       localStorage.setItem('token', token);
       api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      setUserName(name || 'Usuario');
       console.log('Logged in successfully');
     } catch (error) {
       console.error('Error logging in:', error);
@@ -69,7 +72,7 @@ const AdminPage = () => {
 
   const handleAddToCartelera = async () => {
     if (!selectedMovie || !selectedMovie.dbId || !salaId || !startTime) {
-      toast.error('Por favor, completa todos los campos antes de agregar a la cartelera');
+      toast.error('Por favor, completa todos los campos antes de agregar la función a la cartelera');
       return;
     }
 
@@ -85,7 +88,7 @@ const AdminPage = () => {
       fetchCartelera(); // Fetch the updated cartelera
     } catch (error) {
       console.error('Error adding function:', error);
-      if (error.response && error.response.status === 400 && error.response.data.message.includes('horario seleccionado')) {
+      if (error.response && error.response.status === 400) {
         toast.error('Revisa las demás funciones, puede que los horarios estén chocando');
       } else {
         toast.error('Error agregando la función');
@@ -136,96 +139,87 @@ const AdminPage = () => {
   };
 
   return (
-    <Container className="mt-5">
-      <ToastContainer />
-      <Row>
-        <Col>
-          <h1>Admin Page</h1>
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        <Col md={8}>
-          <Form.Group>
-            <Form.Control
-              type="text"
-              placeholder="Search for a movie..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </Form.Group>
-        </Col>
-        <Col md={4}>
-          <Button className="mt-2" onClick={handleSearch}>
-            Search
-          </Button>
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        {searchResults.length > 0 && searchResults.map((movie) => (
-          <Col key={movie.id} md={4} className="mb-4">
-            <Card onClick={() => handleMovieSelect(movie)}>
-              <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-              <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      {selectedMovie && (
-        <Row className="mt-4" ref={movieDetailsRef}>
+    <div>
+      <Header 
+        searchTerm={searchTerm} 
+        setSearchTerm={setSearchTerm} 
+        handleSearch={handleSearch} 
+        userName={userName} 
+      />
+      <Container className="mt-5">
+        <ToastContainer />
+        <Row>
           <Col>
-            <Card>
-              <Card.Body>
-                <Card.Title>{selectedMovie.title}</Card.Title>
-                <Card.Text>{selectedMovie.overview}</Card.Text>
-                <Button className="mt-2" onClick={handleAddMovieToDatabase}>
-                  Agregar a Base de Datos
-                </Button>
-                <Form.Group className="mt-3">
-                  <Form.Label>Sala ID</Form.Label>
-                  <Form.Control
-                    type="number"
-                    value={salaId}
-                    onChange={(e) => setSalaId(e.target.value)}
-                  />
-                </Form.Group>
-                <Form.Group className="mt-2">
-                  <Form.Label>Start Time</Form.Label>
-                  <Form.Control
-                    type="time"
-                    value={startTime}
-                    onChange={(e) => setStartTime(e.target.value)}
-                  />
-                </Form.Group>
-                <Button className="mt-2" onClick={handleAddToCartelera}>
-                  Crear Cartelera
-                </Button>
-              </Card.Body>
-            </Card>
+            <h1>Admin Page</h1>
           </Col>
         </Row>
-      )}
-      <Row className="mt-4">
-        <Col>
-          <h3>Cartelera</h3>
-          {cartelera.length > 0 ? (
-            <ListGroup>
-              {cartelera.map((funcion) => (
-                <ListGroup.Item key={funcion.id}>
-                  {funcion.movie?.name || 'N/A'} - Sala: {funcion.salaId} - Comienza: {formatTime(funcion.startTime)} - Termina: {formatTime(funcion.endTime)} - Estado: {funcion.status}
-                  <Button variant="danger" className="ml-3" onClick={() => handleDeleteFromCartelera(funcion.id)}>
-                    Eliminar
+        <Row className="mt-4">
+          {searchResults.length > 0 && searchResults.map((movie) => (
+            <Col key={movie.id} md={4} className="mb-4">
+              <Card onClick={() => handleMovieSelect(movie)}>
+                <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        {selectedMovie && (
+          <Row className="mt-4" ref={movieDetailsRef}>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>{selectedMovie.title}</Card.Title>
+                  <Card.Text>{selectedMovie.overview}</Card.Text>
+                  <Button className="mt-2" onClick={handleAddMovieToDatabase}>
+                    Agregar a Base de Datos
                   </Button>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          ) : (
-            <p>No functions available</p>
-          )}
-        </Col>
-      </Row>
-    </Container>
+                  <Form.Group className="mt-3">
+                    <Form.Label>Sala ID</Form.Label>
+                    <Form.Control
+                      type="number"
+                      value={salaId}
+                      onChange={(e) => setSalaId(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mt-2">
+                    <Form.Label>Start Time</Form.Label>
+                    <Form.Control
+                      type="time"
+                      value={startTime}
+                      onChange={(e) => setStartTime(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button className="mt-2" onClick={handleAddToCartelera}>
+                    Crear Cartelera
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        )}
+        <Row className="mt-4">
+          <Col>
+            <h3>Cartelera</h3>
+            {cartelera.length > 0 ? (
+              <ListGroup>
+                {cartelera.map((funcion) => (
+                  <ListGroup.Item key={funcion.id}>
+                    {funcion.movie?.name || 'N/A'} - Sala: {funcion.salaId} - Comienza: {formatTime(funcion.startTime)} - Termina: {formatTime(funcion.endTime)} - Estado: {funcion.status}
+                    <Button variant="danger" className="ml-3" onClick={() => handleDeleteFromCartelera(funcion.id)}>
+                      Eliminar
+                    </Button>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            ) : (
+              <p>No functions available</p>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
